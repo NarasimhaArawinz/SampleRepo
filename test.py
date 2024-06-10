bb0e2a5971590eca9a918b10d089d801af461276
NarasimhaArawinz <narasimha@arawinz.com>
2024-06-10 18:24:48 +0530

    forth commit

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import datetime
+
+from pytz import timezone
+
+## with timezone
+def getDateOnly():
+    now = datetime.datetime.now(timezone('Asia/Kolkata'))     # datetime with a particular timezone
+    datetimeee=datetime.datetime(
+            now.year, now.month, now.day,now.hour,now.minute,now.second)    # 
+    datetimee=str(datetimeee)         
+    return datetimee
+
+
+## without timezone
+def getDateOnly():
+    now = datetime.datetime.now()     # datetime based on system's timezone
+    datetimeee=datetime.datetime(
+            now.year, now.month, now.day,now.hour,now.minute,now.second)    # 
+    datetimee=str(datetimeee)         
+    return datetimee
